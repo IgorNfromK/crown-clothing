543280874b74e6397a555c25320421ff2ddc7bf3
IgorNfromK <ccna4igor@gmail.com>
2021-05-04 22:51:15 +0200

    updated shop data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 
-import { Route, Switch, Redirect } from "react-router";
+import { Redirect, Route, Switch } from "react-router";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import {connect} from 'react-redux'
 
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
@@ -10,26 +9,25 @@ import React from "react";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { connect } from "react-redux";
-import {setCurrentUser} from './redux/user/user.actions'
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser}=this.props;
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       // this.setState({ currentUser: user });
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          setCurrentUser({     
-              id: snapShot.id,
-              ...snapShot.data(),
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
       }
-      setCurrentUser( userAuth);
+      setCurrentUser(userAuth);
 
       // createUserProfileDocument(user);
     });
@@ -44,20 +42,31 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route exact path="/signin" redirect={()=>this.props.currentUser? (<Redirect to="/" />) : (<SignInAndSignUpPage>)} />
+          <Route
+            exact
+            path="/signin"
+            redirect={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-const mapStateToProps=({user})=>({
-  currentUser: user.currentUser
-})
-const mapDispatchToProps=dispatch=>({
-  setCurrentUser: user=> dispatch(setCurrentUser(user))
-})
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 
-export default connect(mapDispatchToProps, mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 //npm i node-sass
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,5 @@
- const CartActionTypes={
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
-}
-export default CartActionTypes
+const CartActionTypes = {
+  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+  ADD_ITEM: "ADD_ITEM",
+};
+export default CartActionTypes;
